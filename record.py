5d7f199cdc50460e018fa5a872426aa955190580
Philip Rogers <pdr@chromium.org>
2018-01-07 15:24:38 -0800

    Print errors during merge step instead of raising an exception because these errors can be warnings

--- record.py
+++ record.py
@@ -63,7 +63,7 @@ def record(executable, argsList, verbose):
         proc = subprocess.Popen(command, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
         out, err = proc.communicate()
         if err != "":
-            raise AssertionError(err)
+            print err
         if verbose:
             print out
 
