ed5c37e00f40158b4ef4dc9950b8f3197dc28e98
Philip Rogers <pdr@chromium.org>
2018-01-07 16:03:41 -0800

    Rewrite comparison to use a map keyed by strings for better performance

--- compare.py
+++ compare.py
@@ -9,13 +9,32 @@ from coverage.coverage import Coverage
 
 def compare(coverageA, coverageB):
     differences = []
-    allFunctions = coverageA.functions()
-    for function in coverageB.functions():
-        if function not in allFunctions:
-            allFunctions.append(function)
-    for (file, function) in allFunctions:
-        if coverageA.callCount(file, function) != coverageB.callCount(file, function):
-            differences.append(function + ": was called " + str(coverageA.callCount(file, function)) + " times, not " + str(coverageB.callCount(file, function)) + " times.")
+
+    # Map from file+function to {file, function, aCallCount, bCallCount}.
+    allFunctionsMap = {}
+    for (file, function) in coverageA.functions():
+        key = file + function
+        allFunctionsMap[key] = {}
+        allFunctionsMap[key]["file"] = file
+        allFunctionsMap[key]["function"] = function
+        allFunctionsMap[key]["aCallCount"] = coverageA.callCount(file, function)
+        allFunctionsMap[key]["bCallCount"] = 0
+    for (file, function) in coverageB.functions():
+        key = file + function
+        if key not in allFunctionsMap:
+            allFunctionsMap[key] = {}
+            allFunctionsMap[key]["file"] = file
+            allFunctionsMap[key]["function"] = function
+            allFunctionsMap[key]["aCallCount"] = 0
+        allFunctionsMap[key]["bCallCount"] = coverageB.callCount(file, function)
+
+    for key in allFunctionsMap:
+        aCallCount = allFunctionsMap[key]["aCallCount"]
+        bCallCount = allFunctionsMap[key]["bCallCount"]
+        if aCallCount != bCallCount:
+            file = allFunctionsMap[key]["file"]
+            function = allFunctionsMap[key]["function"]
+            differences.append(file + function + " call count difference: " + str(aCallCount) + " != " + str(bCallCount))
     return differences
 
 def main():
--- test/testCompare.py
+++ test/testCompare.py
@@ -14,12 +14,12 @@ class TestCompare(unittest.TestCase):
         # Compare A vs B.
         differences = compare.compare(coverageA, coverageB)
         self.assertEqual(len(differences), 1)
-        self.assertEqual(differences[0], "fn: was called 5 times, not 0 times.")
+        self.assertEqual(differences[0], "fn call count difference: 5 != 0")
 
         # Compare B vs A.
         differences = compare.compare(coverageB, coverageA)
         self.assertEqual(len(differences), 1)
-        self.assertEqual(differences[0], "fn: was called 0 times, not 5 times.")
+        self.assertEqual(differences[0], "fn call count difference: 0 != 5")
 
     def testFunctionCallCountDifference(self):
         coverageA = Coverage()
@@ -30,12 +30,12 @@ class TestCompare(unittest.TestCase):
         # Compare A vs B.
         differences = compare.compare(coverageA, coverageB)
         self.assertEqual(len(differences), 1)
-        self.assertEqual(differences[0], "fn: was called 5 times, not 3 times.")
+        self.assertEqual(differences[0], "fn call count difference: 5 != 3")
 
         # Compare B vs A.
         differences = compare.compare(coverageB, coverageA)
         self.assertEqual(len(differences), 1)
-        self.assertEqual(differences[0], "fn: was called 3 times, not 5 times.")
+        self.assertEqual(differences[0], "fn call count difference: 3 != 5")
 
     # Integration test using the broken quicksort example.
     def testBrokenQuicksortExample(self):
@@ -45,7 +45,7 @@ class TestCompare(unittest.TestCase):
 
         differences = compare.compare(workingCoverage, brokenCoverage)
         self.assertEqual(len(differences), 1)
-        self.assertEqual(differences[0], "_Z4swapPiii: was called 3 times, not 4 times.")
+        self.assertEqual(differences[0], "_Z4swapPiii call count difference: 3 != 4")
 
 if __name__ == "__main__":
     unittest.main()
