7983f7a17a03fa03d5f725eab840cbd2737b4390
Philip Rogers <pdr@chromium.org>
2018-02-03 07:29:21 -0800

    Cleanup record.py comments and add some basic tests

--- record.py
+++ record.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 
 # record.py - Helper for running a program and recording a raw coverage file.
+#
 # Usage: record.py executable -o coverage.profraw
+#
+# This is just a convenience tool for running an executable that has coverage
+# enabled using the LLVM_PROFILE_FILE environmental variable. For information
+# about doing this manually, see:
+# https://clang.llvm.org/docs/SourceBasedCodeCoverage.html#running-the-instrumented-program
 
 import argparse
 import os.path
 import subprocess
 
-# Run the executable and generate a raw coverage file.
-def recordRawCoverageFile(output, executable, argsList = None, verbose = False):
+# Run the executable and generate a raw coverage file (*.profraw).
+def recordRawCoverageFile(outputFile, executable, argsList = None, verbose = False):
     # Check the executable to ensure it was built with coverage.
     with open (executable, 'r') as inFile:
         executableData = inFile.read()
@@ -16,19 +22,27 @@ def recordRawCoverageFile(output, executable, argsList = None, verbose = False):
     if executableData.count("__llvm_profile") < 10:
         raise AssertionError("No coverage data found in executable. Ensure the \"-fprofile-instr-generate -fcoverage-mapping\" build flags are used.")
 
+    # If the output file already exists, delete it before writing a new one.
+    if os.path.isfile(outputFile):
+        os.remove(outputFile)
+
     # Run the executable and generate a raw coverage file.
     # See: https://clang.llvm.org/docs/SourceBasedCodeCoverage.html#running-the-instrumented-program
     command = [ executable ]
     if argsList:
         command.extend(argsList)
     environment = os.environ.copy()
-    environment["LLVM_PROFILE_FILE"] = output
+    environment["LLVM_PROFILE_FILE"] = outputFile
     proc = subprocess.Popen(command, stderr=subprocess.PIPE, stdout=subprocess.PIPE, env=environment)
     out, err = proc.communicate()
-    if err != "":
-        print err
     if verbose:
+        if err != "":
+            print err
         print out
+
+    # Ensure the raw code coverage file was written.
+    if not os.path.isfile(outputFile):
+        raise AssertionError("Raw code coverage was not saved to \"" + outputFile + "\"" + (": " + err if err else ""))
 
 def main():
     parser = argparse.ArgumentParser(description="Record code coverage")
--- test/testRecord.py
+++ test/testRecord.py
@@ -8,12 +8,37 @@ import record
 class TestRecord(unittest.TestCase):
 
     # Ensure an error is thrown if there is no coverage data in the binary.
-    def testNoCoverageError(self):
+    def testBinaryNotBuiltWithCoverageError(self):
         try:
             tempOutputDir = tempfile.mkdtemp()
             rawCoverageFile = os.path.join(tempOutputDir, "coverage.profraw")
             executable = "test/data/out/noCoverage"
             self.assertRaises(AssertionError, record.recordRawCoverageFile, rawCoverageFile, executable)
+        finally:
+            shutil.rmtree(tempOutputDir)
+
+    # Ensure an error is thrown if the raw coverage file is not written.
+    def testNoCoverageRecordedError(self):
+        try:
+            tempOutputDir = tempfile.mkdtemp()
+            # Use an empty directory for the output file which will not write a
+            # coverage file.
+            rawCoverageFile = tempOutputDir
+            executable = "test/data/out/inlineFunctions"
+            self.assertRaises(AssertionError, record.recordRawCoverageFile, rawCoverageFile, executable)
+        finally:
+            shutil.rmtree(tempOutputDir)
+
+    # Ensure a raw coverage file is written for a simple program.
+    def testCoverageFileWritten(self):
+        try:
+            tempOutputDir = tempfile.mkdtemp()
+            rawCoverageFile = os.path.join(tempOutputDir, "coverage.profraw")
+            # Coverage file should not exist before recording.
+            self.assertFalse(os.path.isfile(rawCoverageFile))
+            executable = "test/data/out/inlineFunctions"
+            record.recordRawCoverageFile(rawCoverageFile, executable)
+            self.assertTrue(os.path.isfile(rawCoverageFile))
         finally:
             shutil.rmtree(tempOutputDir)
 
